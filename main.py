62099620e9ab330ea9144fa7cf85638ea0f13ec6
Samuel Segura <samuel3107@gmail.com>
2020-04-08 18:51:08 +0200

    running automated steps in a loop

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from dao import Dao
 import config
 from gc import collect
 from automatization_steps import Steps
+import logging
 
 network = STAGENET
 
@@ -15,7 +16,29 @@ wallet = WalletRpcClient(network)
 engine = create_engine('mysql+pymysql://{}@{}/{}'.format(config.MYSQL_USER, config.MYSQL_URL, network.mysql_schema))
 dao = Dao(engine)
 bcutil = BlockchainUtils(wallet, daemon, network, dao)
-steps = Steps(bcutil, wallet)
+steps = Steps(bcutil, dao, wallet)
 
-while True:
-    steps.inject(1000)
+logger = logging
+logger.basicConfig(level=config.LOGGING_LEVEL)
+logger.getLogger(__name__)
+
+logger.info('Monero RPC Client started')
+
+
+def do_steps():
+    steps.inject()
+    steps.persist_outputs()
+    steps.persist_rings()
+    steps.mark_my_rings()
+    steps.mark_my_outputs_in_other_rings()
+    # TODO: steps below not yet implemented
+    steps.mark_rings_by_process_of_elimination()
+    steps.mark_outputs_in_deduced_rings()
+    steps.generate_report()
+
+try:
+    while True:
+        do_steps()
+        steps.refresh_heights()
+except KeyboardInterrupt:
+    pass
